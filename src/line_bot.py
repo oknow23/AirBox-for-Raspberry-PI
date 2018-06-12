2b77504cc75b85fffbaf62e781b09a794482ac5e
oknow23 <oknow23@gmail.com>
2018-06-12 17:53:47 +0800

    update line bot

--- src/line_bot.py
+++ src/line_bot.py
@@ -2,9 +2,9 @@ import sys
 
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
-line_bot_api = LineBotApi('MN5xL58/dPw3ovF71cm8acMCOFMtlwtVhQmqM+yjjjqICMhAnEno72xXVHc7APNG8YeQl6gvyqkI5FsjZehBVbQ6gdh95QtHsX+5z39Mu96eOjPK1MyR6KnoejuoTUpUxS0EPFRCV6HFYjykpFnXkQdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('你的Channel access token (long-lived) ')
 
 msg=sys.argv[1]
 
-line_bot_api.push_message('Uff90fb80d93041f1083b2634237c41b1', 
+line_bot_api.push_message('Your user ID', 
      TextSendMessage(text=msg))
